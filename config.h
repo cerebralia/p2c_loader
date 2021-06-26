c4cd51c2dd419c2da0ec964fe58d3ab845799d83
naz <42706009+n4zzu@users.noreply.github.com>
2021-06-26 16:37:16 +0100

    ADDED CONFIG.H

---
+++ config.h
@@ -0,0 +1,31 @@
+//========= Интерфейс
+#define WINDOW_WIDTH  500
+#define WINDOW_HEIGHT 378
+#define CHEAT_NAME "ecstacy"
+#define WCHEAT_NAME L"ecstacy"
+//========= Интерфейс
+
+//========= Лицензия
+#define ENABLE_LICENSING 1
+#define HOST_IP			"YOUR INFO"
+#define HOST			"YOUR INFO"
+#define PATH			"/"
+//========= Лицензия
+
+//========= FTP
+#define FTPHOSTNAME		"YOUR INFO"
+#define FTPLOGIN		"YOUR INFO"
+#define FTPPASSWORD		"YOUR INFO"
+//========= FTP
+
+//========= Не трогай сука
+#define HOST_GATE		"gate.php?serial="
+#define HOST_KEY_GATE	"license-success-ok-"
+#define HOST_CHECK		"check.php?serial="
+#define HOST_KEY_CHECK	"D2DF62F3E61D4696-"
+//========= Не трогай сука
+
+//========= Версия
+#define CHEAT_VERSION	"1"
+//========= Версия
+
